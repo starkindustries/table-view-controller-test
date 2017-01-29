651f813ba4e5ac1ab6a908d1ba86d9abfde994d4
Zion Perez <zionperez@gmail.com>
2017-01-29 15:04:05 +0900

    simplify tableViewController to two sections, each with just one row. Note that now the program will crash when numberOfRowsInSection is greater than 1 (i.e. the number of rows in each section).

--- TableViewControllerTest/ViewController.swift
+++ TableViewControllerTest/ViewController.swift
@@ -23,7 +23,7 @@ class InputTableView: UITableView, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print("tableView numberOfRowsInSection")
-        return 3
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
